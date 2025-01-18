8d712a03211727890bc0bc5fc0a903e05e9c682e
JonayGaroe <jonaygaroe@gmail.com>
2025-01-18 22:43:52 +0000

    Casa 18/1/25
    Arreglos

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,32 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 
 
 public class PlayerController : MonoBehaviour
 {
-
+    // Variables related to player character movement
     public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
 
 
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-        transform.position = position;
     }
 
 
-
-
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
 }
